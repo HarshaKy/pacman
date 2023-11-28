a1b2da98e9b29a7abb64831559bc678ee90d9cb2
HarshaKy <harshaky6573@gmail.com>
2023-11-28 19:30:14 +0000

    stuff

--- MyAgents_v1.py
+++ MyAgents_v1.py
@@ -2,7 +2,7 @@
 
 from pacman import Directions
 from game import Agent
-import api_v2 as api
+import api_v4 as api
 import random
 import game
 import util
@@ -238,6 +238,217 @@ class HungrySurvivorAgent(Agent):
                 return True
         return False
 
-class HungryCornerSeekingSurvivor(Agent):
 
-    None
+class Grid:
+         
+    # Constructor
+    #
+    # Note that it creates variables:
+    #
+    # grid:   an array that has one position for each element in the grid.
+    # width:  the width of the grid
+    # height: the height of the grid
+    #
+    # Grid elements are not restricted, so you can place whatever you
+    # like at each location. You just have to be careful how you
+    # handle the elements when you use them.
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        subgrid = []
+        for i in range(self.height):
+            row=[]
+            for j in range(self.width):
+                row.append(0)
+            subgrid.append(row)
+
+        self.grid = subgrid
+
+    # Print the grid out.
+    def display(self):       
+        for i in range(self.height):
+            for j in range(self.width):
+                # print grid elements with no newline
+                print self.grid[i][j],
+            # A new line after each line of the grid
+            print 
+        # A line after the grid
+        print
+
+    # The display function prints the grid out upside down. This
+    # prints the grid out so that it matches the view we see when we
+    # look at Pacman.
+    def prettyDisplay(self):       
+        for i in range(self.height):
+            for j in range(self.width):
+                # print grid elements with no newline
+                print self.grid[self.height - (i + 1)][j],
+            # A new line after each line of the grid
+            print 
+        # A line after the grid
+        print
+        
+    # Set and get the values of specific elements in the grid.
+    # Here x and y are indices.
+    def setValue(self, x, y, value):
+        self.grid[y][x] = value
+
+    def getValue(self, x, y):
+        return self.grid[y][x]
+
+    # Return width and height to support functions that manipulate the
+    # values stored in the grid.
+    def getHeight(self):
+        return self.height
+
+    def getWidth(self):
+        return self.width
+
+class MyGreedyAgent(Agent):
+
+    def registerInitialState(self, state):
+        self.makeMap(state)
+        self.addWallsToMap(state)
+        self.updateFoodInMap(state)
+        self.map.prettyDisplay()
+    
+    def makeMap(self, state):
+        self.action_map = {
+            'North': (0, 1),
+            'South': (0, -1),
+            'East': (1, 0),
+            'West': (-1, 0),
+            'Stop': (0, 0)
+        }
+        corners = api.corners(state)
+        print corners
+        height = self.getLayoutHeight(corners)
+        width  = self.getLayoutWidth(corners)
+        self.map = Grid(width, height)
+    
+    def getLayoutHeight(self, corners):
+        height = -1
+        for i in range(len(corners)):
+            if corners[i][1] > height:
+                height = corners[i][1]
+        return height + 1
+    
+    def getLayoutWidth(self, corners):
+        width = -1
+        for i in range(len(corners)):
+            if corners[i][0] > width:
+                width = corners[i][0]
+        return width + 1
+
+    def addWallsToMap(self, state):
+        walls = api.walls(state)
+        for i in range(len(walls)):
+            self.map.setValue(walls[i][0], walls[i][1], 1)
+    
+    def updateFoodInMap(self, state):
+        for i in range(self.map.getWidth()):
+            for j in range(self.map.getHeight()):
+                if self.map.getValue(i, j) != 1:
+                    self.map.setValue(i, j, 0)
+        
+        food = api.food(state)
+        for i in range(len(food)):
+            self.map.setValue(food[i][0], food[i][1], 2)
+    
+    # def getAction(self, state):
+    #     self.updateFoodInMap(state)
+    #     self.map.prettyDisplay()
+    #     legal = api.legalActions(state)
+    #     pacman = api.whereAmI(state)
+
+    #     expected_values = []
+    #     for action in legal:
+    #         action_coords = self.action_map[action]
+    #         outcome = self.calculate_outcomes(action_coords, state, pacman)
+    #         expected_value = self.calculate_expected_value(outcome)
+    #         expected_values.append(expected_value)
+        
+    #     max_expected_value_index = expected_values.index(max(expected_values))
+    #     selected_action = legal[max_expected_value_index]
+    #     print "selected action: ", selected_action
+
+    #     return  api.makeMove(selected_action, legal)
+    
+    # def calculate_outcomes(self, action, state, pacman):
+    #     legal = api.legalActions(state)
+
+    #     new_state = []
+    #     probability = 1.0 / len(legal)
+    #     value_at_position = self.map.getValue(pacman[0] + action[0], pacman[1] + action[1])
+        
+    #     return (new_state, probability, value_at_position)
+    
+    # def calculate_expected_value(self, outcome):
+    #     expected_value = 0
+    #     expected_value = outcome[1] * outcome[2]
+        
+    #     return expected_value
+    def getAction(self, state):
+        self.updateFoodInMap(state)
+        self.map.prettyDisplay()
+        legal = api.legalActions(state)
+        pacman = api.whereAmI(state)
+
+        # Check for adjacent food
+        adjacent_food_action = self.get_adjacent_food_action(state, pacman)
+        if adjacent_food_action:
+            print("Eating adjacent food!")
+            return api.makeMove(adjacent_food_action, legal)
+
+        expected_values = []
+        for action in legal:
+            action_coords = self.action_map[action]
+            outcome = self.calculate_outcomes(action_coords, state, pacman)
+            expected_value = self.calculate_expected_value(outcome)
+            expected_values.append(expected_value)
+        
+        max_expected_value_index = expected_values.index(max(expected_values))
+        selected_action = legal[max_expected_value_index]
+        print("Selected action:", selected_action)
+
+        return api.makeMove(selected_action, legal)
+
+    def get_adjacent_food_action(self, state, pacman):
+        # for action in api.legalActions(state):
+        #     action_coords = self.action_map[action]
+        #     successor = state.generateSuccessor(pacman, [action_coords[0], action_coords[1]])
+        #     successor_pacman = api.whereAmI(successor)
+
+        #     if successor_pacman in api.food(successor):
+        #         return action  # Return the action leading to adjacent food
+
+        # return None  # No adjacent food found
+        for action in api.legalActions(state):
+            action_coords = self.action_map[action]
+            if self.map.getValue(pacman[0] + action_coords[0], pacman[1] + action_coords[1]) == 2:
+                return action
+        
+        return random.choice(api.legalActions(state))
+
+    def calculate_outcomes(self, action, state, pacman):
+        legal = api.legalActions(state)
+
+        new_pacman_position = (pacman[0] + action[0], pacman[1] + action[1])
+
+        # if not self.map.is_valid_position(new_pacman_position):
+        #     return None
+
+        probability = 1.0 / len(legal)
+        value_at_position = self.map.getValue(new_pacman_position[0], new_pacman_position[1])
+
+        # Include the new state in the outcome
+        new_state = state.generateSuccessor(pacman, [action[0], action[1]])
+
+        return (new_state, probability, value_at_position)
+
+    def calculate_expected_value(self, outcome):
+        if outcome is None:
+            return 0
+
+        expected_value = outcome[1] * outcome[2]
+        return expected_value
